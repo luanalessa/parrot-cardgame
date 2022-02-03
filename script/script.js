0628f56e2e9e4de23c1c94e8e96986e1f73e2019
Luana Lessa <lessalsn@gmail.com>
2022-02-02 22:41:41 -0300

    chore: Add flip

--- script/script.js
+++ script/script.js
@@ -5,8 +5,8 @@ while (true) {
     quantity = parseInt(prompt("Informe o numero de cartas (entre 4 e 14) "))
     
     if (quantity >= 4 && quantity <= 14 && quantity % 2 == 0) {
-        showCards(quantity)
-        break
+        showCards(quantity);
+        break;
     }
 }
 
@@ -14,7 +14,7 @@ function showCards(number) {
     const board = document.getElementById("board");
 
     for(i = 0; i < number/2; i++) {
-        cards.push(i)
+        cards.push(i);
         cards.push(i); 
     }
 
@@ -22,10 +22,10 @@ function showCards(number) {
 
     for(i = 0; i < number; i++){
         board.innerHTML += `<li class="card">
-                                <p id= ${cards[i]}></p>
-                            </li>`
-    }
-    
+                                <p class="front-face face"></p>
+                                <p class="back-face face" id= ${cards[i]}></p>
+                            </li>`;
+    }   
 }
 
 function shuffle() { 
