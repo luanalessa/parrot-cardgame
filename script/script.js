04a1394be4fc7a00debf594049feb33bc84d27d6
Luana Lessa <lessalsn@gmail.com>
2022-02-02 22:25:06 -0300

    chore: Add shuffled cards

---
+++ script/script.js
@@ -0,0 +1,34 @@
+let quantity = 0;
+let cards = [];
+
+while (true) {
+    quantity = parseInt(prompt("Informe o numero de cartas (entre 4 e 14) "))
+    
+    if (quantity >= 4 && quantity <= 14 && quantity % 2 == 0) {
+        showCards(quantity)
+        break
+    }
+}
+
+function showCards(number) { 
+    const board = document.getElementById("board");
+
+    for(i = 0; i < number/2; i++) {
+        cards.push(i)
+        cards.push(i); 
+    }
+
+    cards.sort(shuffle)
+
+    for(i = 0; i < number; i++){
+        board.innerHTML += `<li class="card">
+                                <p id= ${cards[i]}></p>
+                            </li>`
+    }
+    
+}
+
+function shuffle() { 
+	return Math.random() - 0.5; 
+}
+
